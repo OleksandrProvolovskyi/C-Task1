819e3dd4e991edcb34b8a5bce9a4b62a0a28040e
OleksandrProvolovskyi <47982795+OleksandrProvolovskyi@users.noreply.github.com>
2019-02-27 16:56:44 +0200

    Add files via upload

---
+++ Task1 additional.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.skype.com/uk/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.XPath("/html//a[@id='uhfLogo']")).Click();
+            driver.Close();
+        }
+    }
+}
